e587b8baa9a33f1f59f477d04a52bbce4513b658
Refneda <160188735+Refneda@users.noreply.github.com>
2024-02-16 02:36:53 +0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/av/avmessenger/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/av/avmessenger/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.av.avmessenger;
+package com.Ref.Neda;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.av.avmessenger", appContext.getPackageName());
+        assertEquals("com.ref.neda", appContext.getPackageName());
     }
 }
